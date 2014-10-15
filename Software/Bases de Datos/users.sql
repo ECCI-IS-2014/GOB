bc6a86e96d768e102c93b11643d2f21f2d79e127
unknown <b12037@105-02.ecci.ucr.ac.cr>
2014-10-15 11:23:02 -0600

    users modificado

--- Software/Bases de Datos/users.sql
+++ Software/Bases de Datos/users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 09-10-2014 a las 18:00:26
--- Versión del servidor: 5.6.16
--- Versión de PHP: 5.5.11
+-- Host: 127.0.0.1
+-- Generation Time: Oct 15, 2014 at 07:22 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,37 +17,56 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `tiquiciashop`
+-- Database: `tiquiciashop`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+`id` bigint(20) NOT NULL,
   `first_name` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
   `middle_name` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
   `last_name` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
   `email` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
-  `password` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
-  `identification` int(9) NOT NULL,
+  `password` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `identification` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
   `birth_date` date NOT NULL,
   `username` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
-  `role` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`,`identification`,`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=2 ;
+  `role` varchar(12) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=4 ;
 
 --
--- Volcado de datos para la tabla `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `first_name`, `middle_name`, `last_name`, `email`, `password`, `identification`, `birth_date`, `username`, `role`) VALUES
-(1, 'Daniel', 'Boanerges', 'Mora', 'dan182mora@gmail.com', '$2a$10$t/4VXR8e6JzzZkCR/3qM0.eXHWAncyrUk', 115240499, '1994-12-22', 'Danielmoram', 'admin');
+(1, 'Daniel', 'Boanerges', 'Mora', 'dan182mora@gmail.com', '$2a$10$t/4VXR8e6JzzZkCR/3qM0.eXHWAncyrUk', '115240499', '1994-12-22', 'Danielmoram', 'admin'),
+(2, 'alejandro', 'cordoba', 'soto', 'alk@asdf', '$2a$10$uKUgCRlooWU9reRoIDgPVuA6Lfk3leGl9', '25649', '2014-10-15', 'coba', 'admin'),
+(3, 'alejandro', 'cordoba', 'soto', '54asd@xn--alkj-fqa', '$2a$10$bA7cJHH2p9fzjIuqj2H1XuurCWDTyE9XUukJkzu7YGNkKfvVxMU2m', '16565465', '2014-10-15', 'coba2', 'admin');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `email` (`email`,`identification`,`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
