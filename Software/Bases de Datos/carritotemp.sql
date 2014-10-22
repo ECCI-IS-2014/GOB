563d48e7e6b9a061c8ab6d696b098ccb644f5926
unknown <b11539@105-11.ecci.ucr.ac.cr>
2014-10-22 10:33:34 -0600

    tabla temporal del carrito // Douglas

---
+++ Software/Bases de Datos/carritotemp.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-10-2014 a las 18:30:03
+-- Versión del servidor: 5.6.16
+-- Versión de PHP: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `tiquiciashop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carritotemp`
+--
+
+CREATE TABLE IF NOT EXISTS `carritotemp` (
+  `Id` int(5) NOT NULL AUTO_INCREMENT,
+  `IdProducto` int(10) NOT NULL,
+  `Cantidad` int(10) NOT NULL,
+  `Descripcion` varchar(255) NOT NULL,
+  `dir` varchar(255) NOT NULL,
+  `Precio` int(10) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
