907ca4aed6bd944e6a49556938985fc92e757cee
Alejandro Cordoba Soto <alejandro0608@gmail.com>
2015-05-18 09:35:22 -0600

    SQL
    SQL final hasta el momento

--- OET/Software/OET.sql
+++ OET/Software/OET.sql
@@ -1,4 +1,4 @@
-create table users(
+create table oet_users(
 id number NOT NULL,
 name varchar2(255),
 role varchar2(255),
@@ -8,18 +8,30 @@ password varchar2(255),
 primary key(id)
 );
 
-CREATE TABLE STATIONS (	
-	ID NUMBER NOT NULL , 
-	STATION NUMBER NOT NULL , 
-	DESCRIPTION VARCHAR2(2550), 
-	PRIMARY KEY (ID)
-);
-
 create table data_Types(
 id number not null,
-data_type_id numeric,
+data_type numeric,
 description varchar2(2550),
 temporality VARCHAR2(255),
+primary key(id)
+);
+
+create table manualDatalogs(
+id number not null,
+data_type_id numeric,
+recolection_date timestamp,
+data_ numeric,
+sensor_id number,
+datalog numeric not null,
+station_id numeric,
+
+primary key(id)
+);
+
+create table stations(
+id number not null,
+station number not null,
+description varchar2(2550),
 primary key(id)
 );
 
@@ -38,9 +50,9 @@ provider varchar2(255),
 coordinate_x float,
 coordinate_y float,
 station_id number,
+
 primary key(id)
 );
-
 
 create table features(
 id number NOT NULL,
@@ -62,26 +74,31 @@ sensor_id number,
 constraint PK_logbook primary key(id)
 );
 
-
---select sig from siguientes where tabla = 'usuarios';
-
-INSERT INTO usuarios (name, role, email, password) VALUES ( 'rafa', 'admin', 'da@rafa.com', 'rafa');    
---INSERT INTO usuarios (id, name, role, email, password) VALUES (usuarios_seq.nextval, 'rafa', 'admin', 'da@rafa.com', 'rafa');    
-
 --create sequence usuarios_seq START WITH 20 INCREMENT BY 1;
 create sequence sensors_seq START WITH 1 INCREMENT BY 1;
 create sequence features_seq START WITH 1 INCREMENT BY 1;
 create sequence logbooks_seq START WITH 1 INCREMENT BY 1;
 create sequence data_types_seq START WITH 1 INCREMENT BY 1;
+create sequence manualDatalogs_seq START WITH 1 INCREMENT BY 1;
 create sequence stations_seq START WITH 1 INCREMENT BY 1;
+create sequence oet_users_seq START WITH 1 INCREMENT BY 1;
 
-CREATE OR REPLACE TRIGGER Stations_trigger
+CREATE OR REPLACE TRIGGER stations_trigger
 BEFORE INSERT
 ON stations
 REFERENCING NEW AS NEW
 FOR EACH ROW
 BEGIN
 SELECT stations_seq.nextval INTO :NEW.ID FROM dual;
+END;
+
+CREATE OR REPLACE TRIGGER manualDatalogs_trigger
+BEFORE INSERT
+ON manualDatalogs
+REFERENCING NEW AS NEW
+FOR EACH ROW
+BEGIN
+SELECT manualDatalogs_seq.nextval INTO :NEW.ID FROM dual;
 END;
 
 CREATE OR REPLACE TRIGGER Data_Types_trigger
@@ -93,7 +110,6 @@ BEGIN
 SELECT data_types_seq.nextval INTO :NEW.ID FROM dual;
 END;
 
-
 CREATE OR REPLACE TRIGGER Logbooks_trigger
 BEFORE INSERT
 ON logbooks
@@ -102,8 +118,6 @@ FOR EACH ROW
 BEGIN
 SELECT logbooks_seq.nextval INTO :NEW.ID FROM dual;
 END;
-
-
 
 CREATE OR REPLACE TRIGGER SENSORS_trigger
 BEFORE INSERT
@@ -123,13 +137,13 @@ BEGIN
 SELECT features_seq.nextval INTO :NEW.ID FROM dual;
 END;
 
+CREATE OR REPLACE TRIGGER oet_users_trigger
+BEFORE INSERT
+ON oet_users
+REFERENCING NEW AS NEW
+FOR EACH ROW
+BEGIN
+SELECT oet_users_seq.nextval INTO :NEW.ID FROM dual;
+END;
+
 commit;
-
-create table features(
-sensor_id number NOT NULL,
-name varchar2(255),
-
-primary key(sensor_id)
-);
-
-select * from sensors;
